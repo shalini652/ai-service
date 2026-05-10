b2f3ba34de2fb49f6f0166e44327aca98362439f
shalini652 <shalinimyadav3@gmail.com>
2026-05-10 19:11:32 +0530

    Removed API key and updated Week 4

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -24,10 +27,44 @@ def generate_report():
     data = request.json
     user_input = data.get("input")
 
-    return jsonify({
-        "title": "Risk Report",
-        "summary": f"This report is about {user_input}"
-    })
+    try:
+        response = requests.post(
+            "https://api.groq.com/openai/v1/chat/completions",
+            headers={
+                headers = {
+    "Authorization": "Bearer YOUR_API_KEY",
+    "Content-Type": "application/json"
+}
+            json={
+                "model": "llama-3.3-70b-versatile",
+                "messages": [
+                    {"role": "user", "content": user_input}
+                ]
+            }
+        )
 
-if __name__ == "__main__":
+        result = response.json()
+        ai_output = result["choices"][0]["message"]["content"]
+    
+        
+
+    except:
+        ai_output = "AI not working"
+@app.route('/chat', methods=['POST'])
+def chat():
+
+        data = request.json
+
+        user_message = data.get("message")
+
+        ai_response = f"AI response to:{user_message}"
+        return jsonify({
+            "response": f"You said: {user_message}"
+        })
+
+        return jsonify({
+            "error": str(e)
+        })
+
+if __name__ == '__main__':
     app.run(port=5000)
